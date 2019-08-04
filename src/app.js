4414492081a55c21cd8d210f6957b78840a0b122
Alex <alexander.l.perez@outlook.com>
2019-08-04 11:06:58 -0400

    attempted to solve some of the issues we have

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
-function getInput() {
-    let input = document.getElementById("submit");
-    let output = "You: " + input;
-    sendOutput(output);
+let paragraphCounter = 0;
+
+function startGame() {
+    for (let i = 0; i < 250; i++) {
+        sendOutput("Welcome to Go Home!");
+        
+    }
+    sendOutput("Welcome to Go Home!");
 }
 
+function getInput() {
+    console.log("reached");
+}
+
+function updateOptions(options) {
+    options.forEach(option => {
+        
+    });
+}
+
+/**
+ * Displays the output parameter as text on the page.
+ * @param {string} output The text to display on the page
+ */
 function sendOutput(output) {
-    
+    // Get the last paragraph's Id
+    let lastParagraphId = "p".concat(paragraphCounter++).toString();
+    let lastParagraph = document.getElementById(lastParagraphId);
+
+    // Create a new paragraph as the last one's child
+    let newParagraph = document.createElement("paragraph");
+    newParagraph.setAttribute("class", "log");
+    newParagraph.innerHTML = output;
+    lastParagraph.appendChild(newParagraph);
 }
